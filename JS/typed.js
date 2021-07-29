c57fc6827a7f419df68cef6bb193c502ee0b1595
Dhananjayan SB <dhananjayansb@gmail.com>
2021-07-29 16:13:12 +0530

    update0.2
    Home page updated

---
+++ JS/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    // Waits 1000ms after typing "First"
+    strings: ['Dhananjayan SB', 'a Graduate&#127891;', 'a Developer&#128187;', 'a Quality Analyst&#x1F9EA;', 'a Coffee Lover&#9749;'],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true,
+});
